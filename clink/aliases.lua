5d91bfe6d649e3bbe8572e4abe112bd3b33685f6
amandin <123622161+amawdiin@users.noreply.github.com>
2025-04-06 21:42:53 +0600

    update

--- clink/aliases.lua
+++ clink/aliases.lua
@@ -1,7 +1,12 @@
+local function get_full_path(relative_path)
+    local full_path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") or ""
+    return full_path .. relative_path
+end
+
 local aliases = {
 	nv = "neovide $*",
 	e = "explorer $*",
-	y = "{{yazi_wrapper_path}}"
+  y = get_full_path("y.cmd") .. " $*",
 }
 
 for alias, command in pairs(aliases) do

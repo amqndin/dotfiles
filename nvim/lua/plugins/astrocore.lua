e5362a2dedba4be6914b6a4a8fd149497c0b51f2
amandin <123622161+amawdiin@users.noreply.github.com>
2025-04-10 00:13:25 +0600

    dont change name

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -46,7 +46,7 @@ return {
       },
     },
     commands = {
-      open_messages = {
+      Messages = {
         function()
           local scratch_buffer = vim.api.nvim_create_buf(false, true)
           vim.bo[scratch_buffer].filetype = 'vim'

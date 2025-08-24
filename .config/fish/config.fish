0419dc2eaf433914c25c2a5e46c63e53e2846846
amandin <amawwdin@gmail.com>
2025-08-24 18:03:41 +0600

    Added: .config/equibop/TrayIcons/icon.png Modified: .gitignore Deleted: .dotter/gex.toml .dotter/global.toml .dotter/linux.toml .dotter/windows.toml clink/aliases.lua clink/cdn.cmd clink/fzf.lua clink/starship.lua clink/y.cmd clink/zoxide.lua fastfetch/config.jsonc fastfetch/windows.txt readme.pdf Renamed: iscord/settings/quickCss.css config/equibop/settings/quickCss.css iscord/settings/settings.json config/equibop/settings/settings.json iscord/themes/midnight-catppuccin-mocha.theme.css config/equibop/themes/midnight-catppuccin-mocha.theme.css ish/config.fish config/fish/config.fish ish/fish/config.fish config/fish/fish/config.fish ish/fish/fish_variables config/fish/fish/fish_variables ish/fish_variables config/fish/fish_variables amb/config.toml config/iamb/config.toml anata/kanata-tray.toml config/kanata/kanata-tray.toml anata/kanata.kbd config/kanata/kanata.kbd iri/config.kdl config/niri/config.kdl vim/.neoconf.json config/nvim/.neoconf.json vim/.stylua.toml config/nvim/.stylua.toml vim/assets/presence/jmc.png config/nvim/assets/presence/jmc.png vim/assets/presence/mcfunction.png config/nvim/assets/presence/mcfunction.png vim/init.lua config/nvim/init.lua vim/lua/community.lua config/nvim/lua/community.lua vim/lua/lazy_setup.lua config/nvim/lua/lazy_setup.lua vim/lua/plugins/astrocore.lua config/nvim/lua/plugins/astrocore.lua vim/lua/plugins/astrolsp.lua config/nvim/lua/plugins/astrolsp.lua vim/lua/plugins/astroui.lua config/nvim/lua/plugins/astroui.lua vim/lua/plugins/cord.lua config/nvim/lua/plugins/cord.lua vim/lua/plugins/core.lua config/nvim/lua/plugins/core.lua vim/lua/plugins/heirline.lua config/nvim/lua/plugins/heirline.lua vim/lua/plugins/keymaps.lua config/nvim/lua/plugins/keymaps.lua vim/lua/plugins/mason.lua config/nvim/lua/plugins/mason.lua vim/lua/plugins/navigation.lua config/nvim/lua/plugins/navigation.lua vim/lua/plugins/none-ls.lua config/nvim/lua/plugins/none-ls.lua vim/lua/plugins/obsidian.lua config/nvim/lua/plugins/obsidian.lua vim/lua/plugins/oil.lua config/nvim/lua/plugins/oil.lua vim/lua/plugins/resession.lua config/nvim/lua/plugins/resession.lua vim/lua/plugins/snacks.lua config/nvim/lua/plugins/snacks.lua vim/lua/plugins/treesitter.lua config/nvim/lua/plugins/treesitter.lua vim/lua/plugins/user.lua config/nvim/lua/plugins/user.lua vim/lua/polish.lua config/nvim/lua/polish.lua vim/neovim.yml config/nvim/neovim.yml vim/selene.toml config/nvim/selene.toml tarship/config.toml config/starship.toml ezterm/colors.lua config/wezterm/colors.lua ezterm/wezterm.lua config/wezterm/wezterm.lua ezterm/wezterm_old.lua config/wezterm/wezterm_old.lua

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,13 @@ end
 
 alias nv="nvim"
 alias cat="bat"
+alias cp="cp -iv"
+alias mv="mv -iv"
 alias ls="eza --icons --color=always --group-directories-first"
+
+function cdn
+    mkdir -p $argv[1]; and cd $argv[1]
+end
 
 set -gx QT_QPA_PLATFORM "wayland;xcb"
 set -gx EDITOR "nvim"
--- clink/cdn.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem This batch file creates a directory and then changes into it.
-rem %* captures all arguments passed to the batch file.
-mkdir %*
-if exist %* (
-    cd /d %*
-) else (
-    echo Error: Directory "%*" could not be created or found.
-)

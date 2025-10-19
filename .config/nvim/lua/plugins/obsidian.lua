ee35d6ce44611cfb6c84bd88d0b68310268fbc3d
amandin <amawwdin@gmail.com>
2025-10-19 23:10:17 +0600

    Modified: .config/kanata/kanata.kbd .config/niri/config.kdl .config/nvim/lua/plugins/obsidian.lua

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -58,20 +58,9 @@ return {
         blink = astrocore.is_available "blink",
       },
 
-      disable_frontmatter = true,
-
-      note_frontmatter_func = function(note)
-        -- This is equivalent to the default frontmatter function.
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-        return out
-      end,
+      frontmatter = {
+        enabled = false
+      },
 
       -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
       -- URL it will be ignored but you can customize this behavior here.

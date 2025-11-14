6440e89e9f82318e8942b6092438f4e610c6785e
amandin <amawwdin@gmail.com>
2025-11-14 12:39:29 +0600

    Added: .config/nvim/lua/plugins/snipe.lua Modified: .config/fnott/fnott.ini .config/kanata/kanata.kbd

---
+++ .config/nvim/lua/plugins/snipe.lua
@@ -0,0 +1,16 @@
+return {
+  "leath-dub/snipe.nvim",
+  event = "User AstroFile",
+  keys = {
+    {
+      "<Leader>j",
+      function () require("snipe").open_buffer_menu() end,
+      desc = "Open Snipe buffer menu"
+    }
+  },
+  opts = {
+    hints = {
+      dictionary = "sadfjkl;ghweio",
+    },
+  }
+}

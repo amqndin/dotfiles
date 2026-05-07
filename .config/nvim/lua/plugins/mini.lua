707e94dcc70d842a4319296868ef776269f8f6ba
amandin <amawwdin@gmail.com>
2026-05-07 20:34:54 +0600

    Modified: .config/nvim/lua/plugins/mini.lua

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,17 +3,27 @@
 return {
   {
     'nvim-mini/mini.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects', opts = {}
+    },
     version = false,
     lazy = false,
     config = function()
       local gen_ai_spec = require('mini.extra').gen_ai_spec
+      local spec_treesitter = require('mini.ai').gen_spec.treesitter
 
       require('mini.ai').setup {
         n_lines = 100,
         custom_textobjects = {
-          e = gen_ai_spec.buffer()
+          f = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+          o = spec_treesitter({
+            a = { '@conditional.outer', '@loop.outer' },
+            i = { '@conditional.inner', '@loop.inner' },
+          }),
+          e = gen_ai_spec.buffer(),
         },
       }
+
 
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }

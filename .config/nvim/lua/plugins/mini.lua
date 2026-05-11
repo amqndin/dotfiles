98d4fd5c2f1c28b2544f440d21af4999fcd14b9b
amandin <amawwdin@gmail.com>
2026-05-11 17:43:42 +0600

    big statusline update

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -27,19 +27,42 @@ return {
 
 
       local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          active = function()
+            local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+            local filename = statusline.section_filename({ trunc_width = 140 })
+            local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+            local location = statusline.section_location({ trunc_width = 75 })
+            local search = statusline.section_searchcount({ trunc_width = 75 })
+
+            return statusline.combine_groups({
+              { hl = mode_hl,                  strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo',  strings = { filename } },
+              { hl = 'MiniStatuslineFilename', strings = { '' } },
+              '%=',
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl,                  strings = { search, location } },
+            })
+          end,
+        },
+      }
 
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function() return '%2l:%-2v' end
       statusline.section_filename = function()
-        local result = vim.fn.expand('%:t')
+        local name = vim.fn.expand('%:t')
+        local icon = require('mini.icons').get('file', name)
+        local result = (icon and icon .. ' ' or '') .. name
         if vim.bo.modified then
-          result = "  " .. result
+          result = result .. ' '
         end
         return result
       end
 
       require('mini.icons').setup()
+      ---@diagnostic disable-next-line: undefined-global
       MiniIcons.mock_nvim_web_devicons()
 
       require('mini.pairs').setup()

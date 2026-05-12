d02167bf5c6b668aae96f0482a187d01509e694f
amandin <amawwdin@gmail.com>
2026-05-12 22:21:07 +0600

    Modified: .config/nvim/lua/config/autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,17 +1,29 @@
-local default_cols = vim.o.columns
+local md_group = vim.api.nvim_create_augroup("MarkdownConfig", { clear = true })
+local default_cols
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.md",
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = md_group,
   callback = function()
     default_cols = vim.o.columns
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = md_group,
+  pattern = "*.md",
+  callback = function()
     vim.o.columns = 100
+    vim.opt_local.conceallevel = 2
   end,
 })
 
 vim.api.nvim_create_autocmd("BufLeave", {
+  group = md_group,
   pattern = "*.md",
   callback = function()
-    vim.o.columns = default_cols
+    if default_cols then
+      vim.o.columns = default_cols
+    end
   end,
 })
 

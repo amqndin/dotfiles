ea39a827cac852105a81d36184f8c3ca82af292d
amandin <amawwdin@gmail.com>
2026-05-13 03:16:16 +0600

    Modified: .config/equibop/settings/settings.json .config/nvim/lua/config/autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,19 +1,32 @@
 local md_group = vim.api.nvim_create_augroup("MarkdownConfig", { clear = true })
-local default_cols
+local MD_WIDTH_LIMIT = 100
+local real_terminal_width
 
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd({"VimEnter", "VimResized"}, {
   group = md_group,
   callback = function()
-    default_cols = vim.o.columns
+    vim.defer_fn(function()
+      local not_currently_capped = (vim.o.columns ~= MD_WIDTH_LIMIT)
+      if not_currently_capped then
+        real_terminal_width = vim.o.columns
+        vim.notify("width captured: " .. tostring(real_terminal_width))
+      end
+    end, 50)
   end,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({"BufEnter", "VimResized"}, {
   group = md_group,
   pattern = "*.md",
   callback = function()
-    vim.o.columns = 100
-    vim.opt_local.conceallevel = 2
+    vim.defer_fn(function()
+      local screen_is_wide = (real_terminal_width > MD_WIDTH_LIMIT)
+
+      if real_terminal_width and screen_is_wide then
+        vim.o.columns = MD_WIDTH_LIMIT
+      end
+      vim.opt_local.conceallevel = 2
+    end, 50)
   end,
 })
 
@@ -21,8 +34,8 @@ vim.api.nvim_create_autocmd("BufLeave", {
   group = md_group,
   pattern = "*.md",
   callback = function()
-    if default_cols then
-      vim.o.columns = default_cols
+    if real_terminal_width then
+      vim.o.columns = real_terminal_width
     end
   end,
 })

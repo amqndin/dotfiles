2e76d885990db70d78da447f36482d3b4fe47a0d
amandin <amawwdin@gmail.com>
2026-05-16 11:59:11 +0600

    Added: .config/nvim/lua/plugins/render-markdown.lua Modified: .config/nvim/lua/plugins/cord.lua

--- .config/nvim/lua/plugins/cord.lua
+++ .config/nvim/lua/plugins/cord.lua
@@ -1,12 +1,3 @@
-local blacklist = { }
-
-local function is_blacklisted(opts)
-  for _, workspace in ipairs(blacklist) do
-    if string.find(opts.workspace, workspace) then return true end
-  end
-  return false
-end
-
 return {
   "vyfor/cord.nvim",
   build = ":Cord update",
@@ -14,11 +5,6 @@ return {
   opts = {
     enabled = true,
     log_level = vim.log.levels.INFO,
-    editor = {
-      client = 'neovim',
-      tooltip = 'The Superior Text Editor',
-      icon = nil,
-    },
     display = {
       theme = 'minecraft',
       flavor = 'accent',
@@ -26,43 +12,8 @@ return {
       swap_fields = false,
       swap_icons = false,
     },
-    timestamp = {
-      enabled = true,
-      reset_on_idle = false,
-      reset_on_change = false,
-      shared = false,
-    },
     idle = {
-      enabled = true,
-      timeout = 300000,
-      show_status = true,
-      ignore_focus = true,
-      unidle_on_focus = true,
-      smart_idle = true,
-      details = 'Idling',
-      state = nil,
-      tooltip = '💤',
-      icon = nil,
-    },
-    text = {
-      default = nil,
-      workspace = function(opts)
-        return is_blacklisted(opts) and "In a secret workspace" or ("In " .. opts.workspace)
-      end,
-      viewing = function(opts) return 'Viewing ' .. opts.filename end,
-      editing = function(opts) return 'Editing ' .. opts.filename end,
-      file_browser = function(opts) return 'Browsing files in ' .. opts.name end,
-      plugin_manager = function(opts) return 'Managing plugins in ' .. opts.name end,
-      lsp = function(opts) return 'Configuring LSP in ' .. opts.name end,
-      docs = function(opts) return 'Reading ' .. opts.name end,
-      vcs = function(opts) return 'Committing changes in ' .. opts.name end,
-      notes = function(opts) return 'Taking notes in ' .. opts.name end,
-      debug = function(opts) return 'Debugging in ' .. opts.name end,
-      test = function(opts) return 'Testing in ' .. opts.name end,
-      diagnostics = function(opts) return 'Fixing problems in ' .. opts.name end,
-      games = function(opts) return 'Playing ' .. opts.name end,
-      terminal = function(opts) return 'Executing in ' .. opts.name end,
-      dashboard = 'Home',
+      enabled = false,
     },
     buttons = {
       {

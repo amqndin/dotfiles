c2d3f0626cf05d5cadda30c7574d8d8588cc72cf
amandin <amawwdin@gmail.com>
2026-05-22 20:05:05 +0600

    Modified: .config/niri/dms/binds.kdl .config/niri/layout.kdl scripts/brightness.fish

--- scripts/brightness.fish
+++ scripts/brightness.fish
@@ -1,19 +1,24 @@
 #!/usr/bin/env fish
 
+set BKLIGHT backlight:intel_backlight
+set DDC_MON ddc:i2c-3
+
 function change_brightness
     argparse 'i/inc=' 'd/dec=' -- $argv
     or return 1
 
     if set -q _flag_inc
-        dms ipc call brightness increment $_flag_inc ""
-        ddcutil --model "LG FULL HD" setvcp 10 + $_flag_inc
+        dms ipc call brightness increment $_flag_inc $BKLIGHT
+        dms ipc call brightness increment $_flag_inc $DDC_MON
     else if set -q _flag_dec
-        dms ipc call brightness decrement $_flag_dec ""
-        ddcutil --model "LG FULL HD" setvcp 10 - $_flag_dec
+        dms ipc call brightness decrement $_flag_dec $BKLIGHT
+        dms ipc call brightness decrement $_flag_dec $DDC_MON
     else
         echo "Error: Need argument"
         return 1
     end
 end
 
-change_brightness $argv
+if not status is-interactive
+    change_brightness $argv
+end
